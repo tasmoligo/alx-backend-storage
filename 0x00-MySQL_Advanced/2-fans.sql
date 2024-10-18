8ef651f89de636dc29cf54cdb27537c8ef707812
tasmoligo <tasieibiam28gmail.com>
2024-10-18 06:31:42 +0100

    Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,4 @@
 -- ranks country origins of bands, ordered by the number of (non-unique) fans
-SELECT origin, fans as nb_fans
+SELECT origin, COUNT(fans) as nb_fans
     FROM metal_bands
         ORDER BY nb_fans DESC;
-        
